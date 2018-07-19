d06442cd9e8aef80375a424044144abb4a41ecdc
csetraynor <c.traynor@warwick.ac.uk>
2018-07-19 14:25:24 +0100

    write make script functions for sbatch

---
+++ docs/make_masterscripts.R
@@ -0,0 +1,9 @@
+
+make_maskterscript <- function(){
+
+mastercommand <- sapply(1:100, function(i){
+  print(paste0("sbatch submitscript", i, ".txt;"))
+})
+cat(mastercommand)
+cat(mastercommand, file = "masterscript.txt")
+}
